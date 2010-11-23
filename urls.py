5837da80b4601a8d15ba14058218e8fabaf6fba0
Alexander Bjerkan <alexander.bjerkan@gmail.com>
2010-11-23 18:04:07 +0100

    Introduced design from an old project of mine, using blueprint.css

--- urls.py
+++ urls.py
@@ -1,18 +1,17 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^viktigpedia/', include('viktigpedia.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^', include('viktigpedia.wiki.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':
+            settings.MEDIA_ROOT, 'show_indexes': True}),
+    )
--- wiki/views.py
+++ wiki/views.py
@@ -1,8 +1,19 @@
+from django.conf import settings
+
 from easymode.xslt.response import render_to_response
 from easymode.tree import xml
 
 from wiki.models import Article
 
+#try:
+#    WIKI_LOCK_DURATION = settings.WIKI_LOCK_DURATION
+#except AttributeError:
+#    WIKI_LOCK_DURATION = 15
+#
+#class EditLock():
+
+
 def index(request):
     articles = Article.objects.all()
-    return render_to_response('model-to-xml.xsl', articles)
+    print xml(articles)
+    return render_to_response('base.xsl', articles)
