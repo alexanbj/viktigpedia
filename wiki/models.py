c1522bef6703d4088def943c73a5b78edf1281da
Alexander Bjerkan <alexander.bjerkan@gmail.com>
2010-11-24 17:02:24 +0100

    Expanded article model

--- wiki/models.py
+++ wiki/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+
 from easymode.tree.decorators import toxml
 
 @toxml
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(max_length=200)
+
+    summary = models.TextField()
+    content = models.TextField()
+    creator = models.ForeignKey(User, verbose_name='Article Creator')
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.title
